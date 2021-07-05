e2a22687b90641d37d5dc0071340d6ce4edb2d7c
Ashin007 <ashinvijayan007@gmail.com>
2021-07-06 01:02:47 +0530

    test cases are working fine

--- src/main/java/pageobjectmodel/MainPage.java
+++ src/main/java/pageobjectmodel/MainPage.java
@@ -4,12 +4,20 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
 public class MainPage {
 
     WebDriver driver;
     By cookie_accept = By.xpath("//button[text()='Accept all cookies' and @class='ui-button-primary ui-cookie-accept-all-medium-large']");
     By search_box = By.id("header-location");
     By search_button = By.xpath("//button[@data-testid='search-button']");
+    By price_tag = By.xpath("//p[@size='6' and @class='css-6v9gpl-Text eczcs4p0']");
+    By property_tab = By.xpath("//div[@class='css-wfndrn-StyledContent e2uk8e18']");
+    By agent_link = By.xpath("//div[@class='css-1niyh5b-AgentHeader e11937k17']/a");
+    By agent_name_property = By.xpath("//h3[@size='6' and @class='css-e13akx-Heading3-AgentHeading e11937k16' ]");
+    By agent_name_property_anchor_tag = By.xpath("//h3[@size='6' and @class='css-e13akx-Heading3-AgentHeading e11937k16' ]/a");
+    By agent_name_page = By.xpath("//div[@id='content']/div/h1/b");
 
     public MainPage(WebDriver driver) {
         this.driver = driver;
@@ -24,5 +32,26 @@ public class MainPage {
     }
     public void clickOnSearch(){
         driver.findElement(search_button).click();
+    }
+    public List<WebElement> getAllPriceTags(){
+        return driver.findElements(price_tag);
+    }
+    public List<WebElement> getPropertyTab(){
+        return driver.findElements(property_tab);
+
+    }
+    public void clickOnAgentLink(){
+        driver.findElement(agent_link).click();
+    }
+
+    public String getAgentNameFromPropertyPage(){
+        String parent = driver.findElement(agent_name_property).getText();
+        String child = driver.findElement(agent_name_property_anchor_tag).getText();
+        return parent.replace(child,"").replace("\n","");
+
+    }
+
+    public String getAgentNameFromAgentPage(){
+        return driver.findElement(agent_name_page).getText();
     }
 }
--- src/test/java/testcase/MainTest.java
+++ src/test/java/testcase/MainTest.java
@@ -1,10 +1,15 @@
 package testcase;
 
 import base.Base;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 import pageobjectmodel.MainPage;
+
+import java.util.*;
 
 public class MainTest extends Base {
 
@@ -20,10 +25,46 @@ public class MainTest extends Base {
         mainPage.clickOnCookieAccept();
         mainPage.enterOnSearchBox().sendKeys("London");
         mainPage.clickOnSearch();
+
+        List<WebElement> price_elements = mainPage.getAllPriceTags();
+
+        List<Integer> price_list = new ArrayList<Integer>();
+
+        int price=0;
+
+        for (WebElement element:price_elements){
+            //converting price text -$20,458,21
+            if((Integer.parseInt(element.getText().substring(1).replace(",","")))>0) {
+               price = Integer.parseInt(element.getText().substring(1).replace(",", ""));
+                price_list.add(price);
+            }
+
+        }
+        //Sorting ascending order
+        price_list.sort(Collections.reverseOrder());
+
+        //printing price in ascending order - £500,000
+        for (int amount: price_list){
+            System.out.println("£"+amount);
+        }
+
+        //clicking on the 5th property list
+        mainPage.getPropertyTab().get(5-1).click();
+
+        String agent_name_from_property_page = mainPage.getAgentNameFromPropertyPage();
+
+
+        mainPage.clickOnAgentLink();
+
+
+        String agent_name_from_agent_page = mainPage.getAgentNameFromAgentPage();
+
+
+        Assert.assertEquals(agent_name_from_property_page,agent_name_from_agent_page);
     }
 
     @AfterTest
     public void tearDown(){
-        driver.quit();
+       driver.quit();
     }
 }
