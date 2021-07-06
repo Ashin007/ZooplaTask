2c7124d14d3095373b660f848558f4688ef8085b
Ashin007 <ashinvijayan007@gmail.com>
2021-07-06 22:02:36 +0530

    minor optimization

--- src/test/java/Listners/TestListeners.java
+++ src/test/java/Listners/TestListeners.java
@@ -8,7 +8,7 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-public class TestListners implements ITestListener {
+public class TestListeners implements ITestListener {
     ExtentReports extentReports = ExtentConfig.config();;
     ExtentTest test;
     @Override
